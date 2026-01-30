0f3445c103a3d3ce341eef9705f49c6624b74e73
SAURABH YADAV <saurabhsj069@gmail.com>
2026-01-31 00:17:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,34 @@
- </style>
-    <title>Better Instagram</title>
-  </head>
-  <body>
-    <h1 style="text-align: center">Arrange Images by drag and drop</h1>
-    <div class="flex" id="parent">
-      <div draggable="true" class="image" id="div1">Image 1</div>
-      <div draggable="true" class="image" id="div2">Image 2</div>
-      <div draggable="true" class="image" id="div3">Image 3</div>
-      <div draggable="true" class="image" id="div4">Image 4</div>
-      <div draggable="true" class="image" id="div5">Image 5</div>
-      <div draggable="true" class="image" id="div6">Image 6</div>
-    </div>
-    <script>
-        let draggedElement = null;
+const gridItems = document.querySelectorAll('.grid-item');
 
-        const images = document.querySelectorAll('.image');
-        
-        images.forEach(
-            image=>{
-                //startdragging
-                image.addEventListener('dragstart', function(e){
+let draggedItem = null;
 
-                    draggedElement=image;
-                    // alert(draggedElement)
-                })
+gridItems.forEach(item => {
+    // When dragging starts
+    item.addEventListener('dragstart', (e) => {
+        draggedItem = item;
+        setTimeout(() => {
+            item.classList.add('dragging');
+        }, 0);
+    });
 
-                image.addEventListener('dragenter', function(e){
-                    e.preventDefault();
-                })
+    // When dragging ends
+    item.addEventListener('dragend', () => {
+        draggedItem.classList.remove('dragging');
+        draggedItem = null;
+    });
 
-                image.addEventListener('dragover', function(e){
-                    e.preventDefault();
-                })
+    // When an item is over another
+    item.addEventListener('dragover', (e) => {
+        e.preventDefault(); // Necessary to allow drop
+    });
 
-                // image.addEventListener('dragend', function(e){
-                //     draggedElement=null;
-                //     // alert(draggedElement)
-                // })
-
-                image.addEventListener('drop', function(e){
-
-                    // alert('dropped')
-                    // if(e.target!==draggedElement){
-                    // console.log(getComputedStyle(draggedElement))
-                        const draggedBg = getComputedStyle(draggedElement).backgroundImage;
-                        const targetBg = getComputedStyle(e.target).backgroundImage;
-                        // alert(draggedBg)
-                        //swap
-                        draggedElement.style.backgroundImage=targetBg;
-                        e.target.style.backgroundImage=draggedBg;
-
-                    
-                })
-            }
-        )
-    </script>
-  </body>
-</html>
+    // When an item is dropped on another
+    item.addEventListener('drop', () => {
+        if (draggedItem !== item) {
+            // Swap background images
+            const temp = item.style.backgroundImage;
+            item.style.backgroundImage = draggedItem.style.backgroundImage;
+            draggedItem.style.backgroundImage = temp;
+        }
+    });
+});
